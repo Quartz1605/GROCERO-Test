b0d2089e7e9282da26d23c23bd5e939c6ce7d56f
Quartz1605 <prathambarekal05@gmail.com>
2025-02-08 00:56:23 +0530

    Login page draft-1

--- 01loginpage/src/App.jsx
+++ 01loginpage/src/App.jsx
@@ -4,11 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className='bg-black' style={{fontFamily: 'sans-serif'}}>Ahmad will edit</h1>
+      <form className='bg-black text-amber-50 max-w-2xl w-2xl h-165 rounded-4xl flex justify-center flex-col items-center'>
+        <div className='pb-0'>
+          <img src="https://cdn-icons-png.flaticon.com/128/18438/18438779.png"></img>
+        </div>
+
+        <div className='text-6xl mb-14 pt-0 font-extrabold text-violet-600'>
+          GRÓCERÓ
+        </div>
+
+        <div className='text-4xl font-bold'>
+          Sign in to your account
+        </div>
+        <div className='text-xl text-amber-400'>
+          or start a subscription
+        </div>
+        <input className='bg-amber-100 mt-8 w-115 rounded-lg h-8 text-black p-4 outline-none' placeholder='Email address'></input>
+        <input className='bg-amber-200 mt-1 w-115 rounded-lg h-8 p-4 text-black outline-none' placeholder='Password'></input>
+
+        <div className='pl-75 mt-2 text-amber-50'>forgot your password?</div>
+
+        <button className='text-black mt-7 w-80 bg-amber-400 hover:bg-amber-500'>Sign in</button>
+
+
+
+
+
+      </form>
     </>
   )
 }
