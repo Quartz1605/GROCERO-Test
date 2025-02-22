b82e07a9e78529d5283ce76cf79045369a8f561d
Sanchay Singh <sanchaysingh21@gmail.com>
2025-02-22 16:39:24 +0530

    Login/Signup Changes

--- frontend/src/pages/Login3.jsx
+++ frontend/src/pages/Login3.jsx
@@ -21,7 +21,7 @@ function Login3() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/login/", {
+      const response = await fetch("http://127.0.0.1:8000/api/auth/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
--- frontend/src/pages/Signup3.jsx
+++ frontend/src/pages/Signup3.jsx
@@ -38,7 +38,7 @@ function Signup3() {
     console.log("Submitting data:", payload);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/register/', {
+      const response = await fetch('http://127.0.0.1:8000/api/auth/register/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
