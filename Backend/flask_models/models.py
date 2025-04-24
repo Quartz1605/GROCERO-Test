eb575d784f9e4ee417687502035e6d12b0fdab02
Sanchay Singh <sanchaysingh21@gmail.com>
2025-04-25 00:10:54 +0530

    Added products in django

--- Backend/flask_models/models.py
+++ Backend/flask_models/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-class Products(models.Model):
-    name = models.TextField(primary_key=True,unique=True, blank=True)
-    price = models.TextField(blank=True, null=True)
-    category = models.TextField(blank=True, null=True)
-    last_updated = models.TextField(blank=True, null=True)  
-    img_path = models.TextField(blank=True, null=True)
-    grorates = models.TextField(db_column='groRates', blank=True, null=True)  # Field name made lowercase.
+# class Products(models.Model):
+#     name = models.TextField(primary_key=True,unique=True, blank=True)
+#     price = models.TextField(blank=True, null=True)
+#     category = models.TextField(blank=True, null=True)
+#     last_updated = models.TextField(blank=True, null=True)  
+#     img_path = models.TextField(blank=True, null=True)
+#     grorates = models.TextField(db_column='groRates', blank=True, null=True)  # Field name made lowercase.
 
-    class Meta:
-        managed = False
-        db_table = 'products'
+#     class Meta:
+#         managed = False
+#         db_table = 'products'
 
